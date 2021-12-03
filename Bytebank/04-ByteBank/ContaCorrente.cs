981a89283e181aad6560f77ced6a1c5845876d63
Rainer Ramos <rainermr89@hotmail.com>
2021-12-02 21:05:29 -0300

    Metodos com varios argumentos

--- Bytebank/04-ByteBank/ContaCorrente.cs
+++ Bytebank/04-ByteBank/ContaCorrente.cs
@@ -25,5 +25,19 @@ public class ContaCorrente
         this.saldo += valor;
     }
 
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;  
+        }
+    }
+
 
 }
