854ffbfa89693d98e6d0e0ebc2a16ef46221e183
Rainer Ramos <rainermr89@hotmail.com>
2021-12-10 14:43:01 -0300

    Construtores

--- Bytebank/07-ByteBank/ContaCorrente.cs
+++ Bytebank/07-ByteBank/ContaCorrente.cs
@@ -1,11 +1,28 @@
 ﻿
 
-namespace _06_ByteBank
+namespace _07_ByteBank
 {
     public class ContaCorrente
     {
         public Cliente Titular { get; set; }
-        public int Agencia { get; set; }
+
+        private int _agencia;
+        public int Agencia 
+        {
+            get
+            {
+                return _agencia;    
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
         public int Numero { get; set; }
 
         private double _saldo = 100;
---
+++ Bytebank/07-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.Numero = 45612645;
+            conta.Agencia = 456;
+
+
+            conta.Agencia = -10;
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+
+            Console.ReadLine();
+        }
+    }
+}
