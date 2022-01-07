200076ba96fc722f6c66999e7042c5fdaedfd96a
Rainer <rainermr89@hotmail.com>
2022-01-07 16:04:12 -0300

    Lidando com erros

--- Bytebank/07-ByteBank/Program.cs
+++ Bytebank/07-ByteBank/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _07_ByteBank
+namespace ByteBank
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 867589);
-
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
-
+            Metodo();
             Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
         }
     }
 }
